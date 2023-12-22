39e2107f2c68ddf5c857e14a8d0de6b3c4362981
Sand-raM <umurazawigasabo@gmail.com>
2023-12-22 04:33:38 -0800

    process and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include "sys/types.h"
+#include "stdlib.h"
+#include "unistd.h"
+#include "sys/wait.h"
+
+/**
+ * infinite_while - Runs an infinite while loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Creates five zombie processes
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	pid_t pid;
+	char c = 0;
+
+	while (c < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			c++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (EXIT_SUCCESS);
+}
